7f70e6bf96f976c5f5b10bdfa03f7586112da63e
Creator <samanmaghsoudi67@gmail.com>
2024-05-03 18:05:53 +0430

    show notes table
    todolist

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 #Start with PHP
 #make a login page with PHP
+
 # I log login and create an account with PHP
+
 #it connects to MariaDB with a singleton Desing pattern. also, it inserts new users with insert query on the database.
 #it secures users' passwords with the use md5 hash function.
-
-٫ه
--- home.php
+++ home.php
@@ -1,6 +1,14 @@
 <?
 require_once('main.php');
 $isguest=!isset($_SESSION['email']);
+if(!$isguest){
+    $user_id=$_SESSION['user_id'];
+    $db=DB::getInstance();
+    $records=$db->query("SELECT * FROM x_note WHERE user_id=$user_id");
+
+}else{
+    $records=null;
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -21,12 +29,38 @@ $isguest=!isset($_SESSION['email']);
         <img class="profile-image" src="images/profile-22.png">
         <span style="margin-left: 5px;"><?=_header_guest?><?=_header_wellcome?></span>
         <a   style="margin-left: 5px; text-decoration:none ;" class="btn-blue" href="login.php"><?=_btn_login?></a>
-<?}else{?>
+        <?}else{?>
         <img class="profile-image" src="images/profile-22.png">
         <span style="margin-left: 5px;"><?=$_SESSION['email']?><?=_header_wellcome?></span>
         <a id="#link" style="margin-left: 5px;text-decoration:none;" class="btn-blue" href="logout.php"><?=_btn_logout?></a>
         <?}?>
         </div>
+    </div>
+
+    <div id="content">
+        <?if($isguest){?>
+           <div style="margin-top: 60px; text-align: center; color: #f40; font-size: 20px;">
+           <span>You need to Login in the system to fully use the system!</span>
+           </div>
+          
+       <?}else{?>
+        <ul class="todo-entry">
+            <li>Title</li>
+            <li>Description</li>
+            <li>Time</li>
+
+        </ul>
+           <?foreach ($records as $record) {?>
+        <ul class="todo-entry">
+            <li><?=$record['title']?></li>
+            <li><?=$record['description']?></li>
+            <li><?=$record['eventTime']?></li>
+
+        </ul>
+
+            <?}?>
+        <? }?> 
+
     </div>
 </body>
 </html>
